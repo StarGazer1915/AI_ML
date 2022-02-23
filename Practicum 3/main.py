362340825afa051394f7aa1724f656c62fc7457a
StarGazer1915 <74311615+StarGazer1915@users.noreply.github.com>
2022-02-23 22:03:09 +0100

    Practicum 3: Copied perceptron work for setup Neuron

---
+++ Practicum 3/main.py
@@ -0,0 +1,75 @@
+from neuron import Neuron, NeuronLayer, NeuronNetwork
+
+# =============== DEFINING POSSIBILITIES =============== #
+pos_1x = [[1], [0]]
+pos_2x = [[1, 1], [1, 0], [0, 1], [0, 0]]
+pos_3x = [[1, 1, 1], [0, 1, 1], [1, 1, 0],
+          [0, 0, 1], [0, 1, 0], [1, 0, 0],
+          [1, 0, 1], [0, 0, 0]]
+
+# =============== DEFINING PERCEPTRONS =============== #
+# p1 = Perceptron("AND", [0.5, 0.5], -1)
+# p2 = Perceptron("OR", [0.1, 0.1], -0.1)
+# p3 = Perceptron("NOT", [-1], 0.5)
+# p4 = Perceptron("Figuur 2.8", [0.6, 0.3, 0.2], -0.4)
+# p5 = Perceptron("NOR 3x", [-1, -1, -1], 0)
+# p6 = Perceptron("NOR 2x", [-1, -1], 0)
+# p7 = Perceptron("NAND", [-0.1, -0.1], 0.1)
+# p8 = Perceptron("PASSTROUGH", [0, 0, 1], -0.1)
+# p9 = Perceptron("HA-AND", [0.5, 0.5, 0], -1)
+
+# =============== DEFINING NETWORK [XOR] =============== #
+# pLayer1_1 = PerceptronLayer('NAND, OR', [p7, p2])
+# pLayer1_2 = PerceptronLayer('AND', [p1])
+# pNetwork1 = PerceptronNetwork('XOR', [pLayer1_1, pLayer1_2])
+
+# =============== DEFINING NETWORK [HALF ADDER] =============== #
+# pLayer2_1 = PerceptronLayer('NAND, OR, AND', [p7, p2, p1])
+# pLayer2_2 = PerceptronLayer('HA-AND, PASSTROUGH', [p9, p8])
+# pNetwork2 = PerceptronNetwork('HALF ADDER', [pLayer2_1, pLayer2_2])
+
+
+# ===============  TEST FUNCTIONS =============== #
+def test_neuron(n, inputs, expected):
+    """
+    This function tests the perceptron by comparing the output and expected values.
+    The print statements and returned output from the 'execute_batch' method will show
+    if the outputs are what is expected from the perceptor.
+    @param p: perceptor object
+    @param inputs: list
+    @param expected: list
+    @return: list
+    """
+    print(f"\n{n}")
+    return n.execute_batch(inputs, expected)
+
+
+def test_network(net, inputs, expected):
+    """
+    Mostly same as the test_perceptor() function but for a network.
+    The function also prints the layers of the network to visualize it's structure.
+    @param net: network object
+    @param inputs: list
+    @param expected: list
+    @return: list
+    """
+    print(f"\n{net}")
+    for layer in net.get_layers():
+        print(layer)
+    return net.feed_forward(inputs, expected)
+
+
+# =============== TEST CASES PERCEPTORS =============== #
+# test_perceptor(p1, pos_2x, [1, 0, 0, 0])
+# test_perceptor(p2, pos_2x, [1, 1, 1, 0])
+# test_perceptor(p3, pos_1x, [0, 1])
+# test_perceptor(p4, pos_3x, [1, 1, 1, 0, 0, 1, 1, 0])
+# test_perceptor(p5, pos_3x, [0, 0, 0, 0, 0, 0, 0, 1])
+# test_perceptor(p6, pos_2x, [0, 0, 0, 1])
+# test_perceptor(p7, pos_2x, [0, 1, 1, 1])
+# test_perceptor(p8, pos_3x, [1, 1, 0, 1, 0, 0, 1, 0])
+# test_perceptor(p9, pos_3x, [1, 0, 1, 0, 0, 0, 0, 0])
+
+# =============== TEST CASES NETWORKS =============== #
+# test_network(pNetwork1, pos_2x, [[0], [1], [1], [0]])
+# test_network(pNetwork2, pos_2x, [[0, 1], [1, 0], [1, 0], [0, 0]])
---
+++ Practicum 3/neuron.py
@@ -0,0 +1,131 @@
+
+class Neuron:
+    def __init__(self, n_type, weights, b):
+        """
+        @param p_type: string
+        @param weights: list
+        @param b: int / float
+        """
+        self.p_type = n_type
+        self.weights = weights
+        self.b = b
+
+    def execute_batch(self, inputs, expected):
+        """
+        The main function to run multiple activations.
+        The function runs an activation for each input given and generates an output. The output is then
+        compared and put into a print statement to give an accurate visual representation of the output
+        and if it's correct/expected. All the outputs are then returned as a list.
+        @param inputs: list
+        @param expected: list
+        @return: list
+        """
+        total_outputs = []
+        if len(inputs) == len(expected):
+            for i in range(len(inputs)):
+                output = self.activation(inputs[i])
+                total_outputs.append(output)
+                correct = output == expected[i]
+                print(f"[{self.n_type}] | Input: {inputs[i]} | Output: {output} | Correct: {correct}")
+        else:
+            print("Length of inputs and expected are not equal.")
+
+        return total_outputs
+
+    def activation(self, inputs):
+        """
+        This function handles the activation of the perceptron.
+        The function uses the sigmoid function to determine the output.
+        @param inputs: list
+        @return: int
+        """
+        sum = self.b
+        for i in range(len(inputs)):
+            sum += inputs[i] * self.weights[i]
+
+        # if sum
+
+    def __str__(self):
+        """
+        Function to make the perceptron object printable for informational purposes.
+        @return: string
+        """
+        return f"Perceptron | Type: '{self.n_type}' | Weights: '{self.weights}' | Bias: '{self.b}'"
+
+
+class NeuronLayer:
+    def __init__(self, n_type, ptrons):
+        """
+        @param p_type: string
+        @param ptrons: list
+        """
+        self.p_type = n_type
+        self.ptrons = ptrons
+
+    def activation(self, inputs):
+        """
+        This function handles the activation of the perceptrons that are in the layer.
+        This function uses the step function from the activation() function in the perceptron class.
+        @param inputs: list
+        @return: list
+        """
+        output = []
+        for perceptron in self.ptrons:
+            output.append(perceptron.activation(inputs))
+
+        return output
+
+    def __str__(self):
+        """
+        Function to make the perceptron layer object printable for informational purposes.
+        @return: string
+        """
+        return f"Perceptron Layer | Amount of Perceptrons: '{len(self.ptrons)}' | Perceptron types: '{self.n_type}'"
+
+
+class NeuronNetwork:
+    def __init__(self, net_type, pLayers):
+        """
+        @param net_type: string
+        @param pLayers: list
+        """
+        self.net_type = net_type
+        self.pLayers = pLayers
+
+    def feed_forward(self, inputs, expected):
+        """
+        The main function to run multiple activations in multiple layers.
+        The function runs an activation for each input given and generates an output. The output is then
+        compared and put into a print statement to give an accurate visual representation of the output
+        and if it's correct/expected. All the outputs are then returned as a list.
+        @param inputs: list
+        @param expected: list
+        @return: list
+        """
+        total_outputs = []
+        if len(inputs) == len(expected):
+            for i in range(len(inputs)):
+                output_value = inputs[i]
+                for layer in self.pLayers:
+                    output_value = layer.activation(output_value)
+                total_outputs.append(output_value)
+                correct = output_value == expected[i]
+                print(f"[{self.net_type}] | Input: {inputs[i]} | Output: {output_value} | Correct: {correct}")
+        else:
+            print("Length of inputs and expected are not equal.")
+
+        return total_outputs
+
+    def get_layers(self):
+        """
+        Function that returns the layers the network holds.
+        @return: list
+        """
+        return self.pLayers
+
+    def __str__(self):
+        """
+        Function to make the perceptron network printable for informational purposes.
+        @return: string
+        """
+        return f"Perceptron Network | Type: '{self.net_type}' | Amount of Layers: '{len(self.pLayers)}'"
